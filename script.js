7229337a621095b1dce75854d685ba2479f52b28
Sergio García <57625297+sergiogarciiam@users.noreply.github.com>
2023-02-24 17:39:12 +0100

    Add compareResult and testEndGame functions

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ options.forEach((option) => {
   option.addEventListener("click", (e) => {
     addUserResult(e);
     addComputerResult();
+    compareResult();
+    testEndGame();
   });
 });
 
 function addUserResult(e) {
   var userResult = document.querySelector(".user-result");
 
-  if (userResult.classList.length != 1) {
-    userResult.classList.remove(userResult.classList[1]);
+  if (userResult.classList.length > 2) {
     userResult.classList.remove(userResult.classList[2]);
   }
-  userResult.classList.add(e.target.classList[0]);
   userResult.classList.add(e.target.classList[1]);
 }
 
@@ -22,10 +22,9 @@ function addComputerResult() {
   var computerResult = document.querySelector(".computer-result");
   var classComputerResult = computerResult.classList;
 
-  if (classComputerResult.length > 1) {
-    classComputerResult.remove(classComputerResult[1]);
+  if (classComputerResult.length > 2) {
+    classComputerResult.remove(classComputerResult[2]);
   }
-  classComputerResult.add("fa-solid");
 
   var random = getRandom();
   switch (random) {
@@ -42,6 +41,43 @@ function addComputerResult() {
       break;
     default:
       console.log(random);
+  }
+}
+
+function compareResult() {
+  var userResult = document.querySelector(".user-result");
+  var userPoints = document.querySelector(".user-points");
+  var classUserResult = userResult.classList[2];
+
+  var computerResult = document.querySelector(".computer-result");
+  var computerPoints = document.querySelector(".computer-points");
+  var classComputerResult = computerResult.classList[2];
+
+  if (classUserResult === classComputerResult) {
+    console.log("tie");
+  } else if (
+    (classUserResult === "fa-hand-fist" &&
+      classComputerResult === "fa-hand-scissors") ||
+    (classUserResult === "fa-hand" && classComputerResult === "fa-hand-fist") ||
+    (classUserResult === "fa-hand-scissors" &&
+      classComputerResult === "fa-hand")
+  ) {
+    userPoints.textContent = +userPoints.textContent + 1;
+    console.log("user win");
+  } else {
+    computerPoints.textContent = +computerPoints.textContent + 1;
+    console.log("computer win");
+  }
+}
+
+function testEndGame() {
+  var userPoints = document.querySelector(".user-points");
+  var computerPoints = document.querySelector(".computer-points");
+
+  if (userPoints.textContent == 5 || computerPoints == 5) {
+    userPoints.textContent = 0;
+    computerPoints.textContent = 0;
+    console.log("end game");
   }
 }
 
